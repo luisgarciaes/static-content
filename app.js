0dd22a8ad4ac7b774afe914c71a65c9ae081e04b
ruruisu <zaddozgl@gmail.com>
2022-03-26 21:39:07 -0600

    style sheet link

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(`<html><head><link rel="stylesheet" href="./style.css"></head><body><h1>Hola mundito</h1></body></html>`);
+});
+app.get("/api", (req, res) => {
+  res.json({ firstname: "Luis", lastname: "Garcia" });
+});
+app.get("/person/:id", (req, res) => {
+  res.send(
+    "<html><head></head><body><h1>Person: " +
+      req.params.id +
+      "</h1></body></html>"
+  );
+});
+app.listen(port);
+
+app.use("/assets", express.static(__dirname + "/public"));
